c2643411883392e0fd17f7130b71347c430fbc04
Anmol Arora <heymailme04@gmail.com>
2024-08-20 18:53:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import cors from 'cors'
+
+const server = express();
+let PORT = 3000;
+
+server.use(express.json());
+
+server.listen(PORT, () => {
+    console.log("Listening on " + PORT);
+});
+
